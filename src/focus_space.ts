004b131e7f78f4ea842ed8c2d2db83a94cf1476d
rosenpin <mail@tomerrosenfeld.com>
2024-01-02 17:35:18 +0100

    use library built in type instead of writing my own

--- src/focus_space.ts
+++ src/focus_space.ts
@@ -1,7 +1,7 @@
-import { showHUD } from "@raycast/api";
+import { LaunchProps, showHUD } from "@raycast/api";
 import { execYabaiCommand } from './utils';
 
-export default async function main(args: { arguments: { spaceIndex: string } }) {
+export default async function main(args: LaunchProps) {
     console.log(args.arguments.spaceIndex);
     const spaceIndex = parseInt(args.arguments.spaceIndex, 10);
     if (isNaN(spaceIndex)) {
