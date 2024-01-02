216c6d1bc2a75a937569d2fcf618b0f3721932a9
rosenpin <mail@tomerrosenfeld.com>
2024-01-02 14:36:48 +0100

    reordered destroy operation

--- src/create_space_and_focus.ts
+++ src/create_space_and_focus.ts
@@ -25,7 +25,7 @@ export default async function main() {
     await execAsync(`${yabaiPath} -m space --focus ${lastSpaceIndex}`, { env: { ...process.env, USER: username } });
 
     // Show HUD notification
-    await showHUD(`Focused space index: ${lastSpaceIndex}`);
+    await showHUD(`Created space: ${lastSpaceIndex}`);
   } catch (error) {
     console.error("Error executing yabai commands", error);
     await showHUD(`Error: ${error.message}`);
--- src/destroy_space.ts
+++ src/destroy_space.ts
@@ -14,9 +14,10 @@ export default async function main() {
     const yabaiPath = process.arch === "arm64" ? "/opt/homebrew/bin/yabai" : "/usr/local/bin/yabai";
 
     // Create a new space
-    await execAsync(`${yabaiPath} -m space --destroy && ${yabaiPath} -m space --focus recent`, { env: { ...process.env, USER: username } });
+    await execAsync(`${yabaiPath} -m space --destroy`, { env: { ...process.env, USER: username } });
     // Show HUD notification
     await showHUD(`Destroyed Space`);
+    await execAsync(`${yabaiPath} -m space --focus recent`, { env: { ...process.env, USER: username } });
   } catch (error) {
     console.error("Error executing yabai commands", error);
     await showHUD(`Error: ${error.message}`);
